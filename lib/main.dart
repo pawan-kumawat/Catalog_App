65f11bfc92888eeda1b0dad816b18c855cf7d5e2
pawan <realmebook950@gmail.com>
2022-07-31 13:21:21 +0530

    Day4 cpmpleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(Myapp());
@@ -22,8 +23,8 @@ class Myapp extends StatelessWidget {
           initialRoute:"/",
          routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage()
+        Myroutes.homeRoute :(context) => HomePage(),
+        Myroutes.loginRoute: (context) => LoginPage()
       },
     );
   }
---
+++ lib/utils/routes.dart
@@ -0,0 +1,4 @@
+class Myroutes {
+  static String loginRoute = "/login";
+  static String homeRoute = "/home";
+}
