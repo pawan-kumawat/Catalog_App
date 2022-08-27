98fb2790db470b542e684ae4964d9be34bb350d2
pawan <realmebook950@gmail.com>
2022-08-27 14:44:07 +0530

    Day15 completed

---
+++ lib/pages/home_details_pages.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomeDetailPage extends StatelessWidget {
+  final Item catalog;
+
+  const HomeDetailPage({super.key, required this.catalog})
+      : assert(catalog != null);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: MyTheme.creamColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Hero(
+                tag: Key(catalog.id.toString()),
+                child: Image.network(catalog.image)),
+          ],
+        ).p16(),
+      ),
+    );
+  }
+}
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,4 +1,5 @@
 class Myroutes {
   static String loginRoute = "/login";
   static String homeRoute = "/home";
+  static String homeDetailsRoute = "/detail";
 }
